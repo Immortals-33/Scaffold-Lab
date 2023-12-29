7f6e0c909a21480be1feec393bed619a4222d919
Immortals-33 <1332011796@qq.com>
2023-12-29 20:46:01 +0800

    Refolding pipeline on RFdiffusion and Chroma is already set-up, but 6VW1(for both) and 5IUS(for chroma) has some bugs, needed to be checked in a few days

--- analysis/csv_merge.py
+++ analysis/csv_merge.py
@@ -3,11 +3,11 @@ import os
 
 merged_data = pd.DataFrame()
 
-for root, dirs, files in os.walk('/dssg/home/acct-clschf/clschf/zzq/se3_diffusion/inference_outputs/21D_05M_2023Y_16h_28m_08s/'):
+for root, dirs, files in os.walk('/dssg/home/acct-clschf/clschf/zzq/Scaffold-Lab/refolding/Framediff/unconditional/'):
     for file in files:
         if file == 'sc_results.csv':
             csv_path = os.path.join(root, file)
             df = pd.read_csv(csv_path)
             merged_data = pd.concat([merged_data, df], ignore_index=True)
             
-merged_data.to_csv('../unscore_results.csv', index=False)
+merged_data.to_csv('../score_results.csv', index=False)
