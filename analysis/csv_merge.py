efa8cc97566215abe2fffc0d80d58db21d51b783
Immortals-33 <1332011796@qq.com>
2023-12-31 05:32:11 +0800

    Motif-scaffolding pipeline done, but still a little bug unfixed. Wish me a happy new year:)

--- analysis/csv_merge.py
+++ analysis/csv_merge.py
@@ -3,11 +3,11 @@ import os
 
 merged_data = pd.DataFrame()
 
-for root, dirs, files in os.walk('/dssg/home/acct-clschf/clschf/zzq/Scaffold-Lab/refolding/Framediff/unconditional/'):
+for root, dirs, files in os.walk('/dssg/home/acct-clschf/clschf/zzq/methods/chroma/benchmark/motif_scaffolding/'):
     for file in files:
-        if file == 'sc_results.csv':
+        if file == 'backbone_results.csv':
             csv_path = os.path.join(root, file)
             df = pd.read_csv(csv_path)
             merged_data = pd.concat([merged_data, df], ignore_index=True)
             
-merged_data.to_csv('../score_results.csv', index=False)
+merged_data.to_csv('../total_results.csv', index=False)
