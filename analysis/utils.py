8687ec4e170af450085100188bd29969ea8775d4
Immortals-33 <1332011796@qq.com>
2024-03-25 16:36:44 +0800

    :new: Update utils and scripts for unconditional generation

--- analysis/utils.py
+++ analysis/utils.py
@@ -16,6 +16,7 @@ import biotite.application.dssp as dssp
 from biotite.structure.residues import get_residues
 from biotite.structure import get_chains
 from biotite.sequence import ProteinSequence
+from tmtools import tm_align
 
 
 def motif_extract(
@@ -119,6 +120,112 @@ def radius_of_gyration(
         c_alpha = t.select_atoms("name CA")
         rg = c_alpha.atoms.radius_of_gyration()
     return round(rg, 3)
+
+
+def calc_tm_score(pos_1, pos_2, seq_1, seq_2):
+    tm_results = tm_align(pos_1, pos_2, seq_1, seq_2)
+    return tm_results.tm_norm_chain1, tm_results.tm_norm_chain2
+
+def calc_aligned_rmsd(pos_1, pos_2):
+    aligned_pos_1 = rigid_transform_3D(pos_1, pos_2)[0]
+    return np.mean(np.linalg.norm(aligned_pos_1 - pos_2, axis=-1))
+
+def rigid_transform_3D(A, B, verbose=False):
+    # Transforms A to look like B
+    # https://github.com/nghiaho12/rigid_transform_3D
+    assert A.shape == B.shape
+    A = A.T
+    B = B.T
+
+    num_rows, num_cols = A.shape
+    if num_rows != 3:
+        raise Exception(f"matrix A is not 3xN, it is {num_rows}x{num_cols}")
+
+    num_rows, num_cols = B.shape
+    if num_rows != 3:
+        raise Exception(f"matrix B is not 3xN, it is {num_rows}x{num_cols}")
+
+    # find mean column wise
+    centroid_A = np.mean(A, axis=1)
+    centroid_B = np.mean(B, axis=1)
+
+    # ensure centroids are 3x1
+    centroid_A = centroid_A.reshape(-1, 1)
+    centroid_B = centroid_B.reshape(-1, 1)
+
+    # subtract mean
+    Am = A - centroid_A
+    Bm = B - centroid_B
+
+    H = Am @ np.transpose(Bm)
+
+    # sanity check
+    #if linalg.matrix_rank(H) < 3:
+    #    raise ValueError("rank of H = {}, expecting 3".format(linalg.matrix_rank(H)))
+
+    # find rotation
+    U, S, Vt = np.linalg.svd(H)
+    R = Vt.T @ U.T
+
+    # special reflection case
+    reflection_detected = False
+    if np.linalg.det(R) < 0:
+        if verbose:
+            print("det(R) < R, reflection detected!, correcting for it ...")
+        Vt[2,:] *= -1
+        R = Vt.T @ U.T
+        reflection_detected = True
+
+    t = -R @ centroid_A + centroid_B
+    optimal_A = R @ A + t
+
+    return optimal_A.T, R, t, reflection_detected
+
+def parse_pdb_feats(
+        pdb_name: str,
+        pdb_path: str,
+        scale_factor=1.,
+        chain_id='A',
+    ):
+    """
+    Only used in inference procedure to prepare ESMFold prediction.
+    
+    Args:
+        pdb_name: name of PDB to parse.
+        pdb_path: path to PDB file to read.
+        scale_factor: factor to scale atom positions.
+        mean_center: whether to mean center atom positions.
+    Returns:
+        Dict with CHAIN_FEATS features extracted from PDB with specified
+        preprocessing.
+    """
+    parser = PDB.PDBParser(QUIET=True)
+    structure = parser.get_structure(pdb_name, pdb_path)
+    struct_chains = {
+        chain.id: chain
+        for chain in structure.get_chains()}
+
+    def _process_chain_id(x):
+        chain_prot = process_chain(struct_chains[x], x)
+        chain_dict = dataclasses.asdict(chain_prot)
+
+        # Process features
+        feat_dict = {x: chain_dict[x] for x in CHAIN_FEATS}
+        return parse_chain_feats(
+            feat_dict, scale_factor=scale_factor)
+
+    if isinstance(chain_id, str):
+        return _process_chain_id(chain_id)
+    elif isinstance(chain_id, list):
+        return {
+            x: _process_chain_id(x) for x in chain_id
+        }
+    elif chain_id is None:
+        return {
+            x: _process_chain_id(x) for x in struct_chains
+        }
+    else:
+        raise ValueError(f'Unrecognized chain list {chain_id}')
 
 def randomly_select_and_copy_pdb_files(src_folder, dst_folder, num_files):
     """
---
+++ data/structure_utils.py
@@ -0,0 +1,215 @@
+import os
+import numpy as np
+import collections
+import dataclasses
+import string
+import io
+import gzip
+from typing import List, Dict, Any
+from scipy.spatial.transform import Rotation
+
+import torch
+from torch.utils import data
+
+from Bio import PDB
+from Bio.PDB.Chain import Chain
+from tmtools import tm_align
+
+from data import residue_constants
+from data import protein
+
+
+"""
+Helpful functions to deal with protein structure data.
+Code inside this script mainly from https://github.com/jasonkyuyim/se3_diffusion/blob/master/data/utils.py
+"""
+
+
+Protein = protein.Protein
+
+
+CHAIN_FEATS = [
+    'atom_positions', 'aatype', 'atom_mask', 'residue_index', 'b_factors'
+]
+
+
+aatype_to_seq = lambda aatype: ''.join([
+        residue_constants.restypes_with_x[x] for x in aatype])
+
+
+def parse_pdb_feats(
+        pdb_name: str,
+        pdb_path: str,
+        scale_factor=1.,
+        chain_id='A',
+    ):
+    """
+    Only used in inference procedure to prepare ESMFold prediction.
+    
+    Args:
+        pdb_name: name of PDB to parse.
+        pdb_path: path to PDB file to read.
+        scale_factor: factor to scale atom positions.
+        mean_center: whether to mean center atom positions.
+    Returns:
+        Dict with CHAIN_FEATS features extracted from PDB with specified
+        preprocessing.
+    """
+    parser = PDB.PDBParser(QUIET=True)
+    structure = parser.get_structure(pdb_name, pdb_path)
+    struct_chains = {
+        chain.id: chain
+        for chain in structure.get_chains()}
+
+    def _process_chain_id(x):
+        chain_prot = process_chain(struct_chains[x], x)
+        chain_dict = dataclasses.asdict(chain_prot)
+
+        # Process features
+        feat_dict = {x: chain_dict[x] for x in CHAIN_FEATS}
+        return parse_chain_feats(
+            feat_dict, scale_factor=scale_factor)
+
+    if isinstance(chain_id, str):
+        return _process_chain_id(chain_id)
+    elif isinstance(chain_id, list):
+        return {
+            x: _process_chain_id(x) for x in chain_id
+        }
+    elif chain_id is None:
+        return {
+            x: _process_chain_id(x) for x in struct_chains
+        }
+    else:
+        raise ValueError(f'Unrecognized chain list {chain_id}')
+
+
+def parse_chain_feats(chain_feats, scale_factor=1.):
+    ca_idx = residue_constants.atom_order['CA'] # 1
+    chain_feats['bb_mask'] = chain_feats['atom_mask'][:, ca_idx] # backbone mask 只考虑CA
+    bb_pos = chain_feats['atom_positions'][:, ca_idx] # 提取backbone(CA)坐标
+    bb_center = np.sum(bb_pos, axis=0) / (np.sum(chain_feats['bb_mask']) + 1e-5) #计算骨架中心坐标 (先求和，再除以CA数量)
+    centered_pos = chain_feats['atom_positions'] - bb_center[None, None, :] # 骨架中心化(将所有CA减去中心坐标)
+    scaled_pos = centered_pos / scale_factor 
+    chain_feats['atom_positions'] = scaled_pos * chain_feats['atom_mask'][..., None] # 更新所有原子的坐标
+    chain_feats['bb_positions'] = chain_feats['atom_positions'][:, ca_idx] # 更新骨架原子坐标
+    return chain_feats
+
+
+def process_chain(chain: Chain, chain_id: str) -> Protein:
+    """Convert a PDB chain object into a AlphaFold Protein instance.
+
+    Forked from alphafold.common.protein.from_pdb_string
+
+    WARNING: All non-standard residue types will be converted into UNK. All
+        non-standard atoms will be ignored.
+
+    Took out lines 94-97 which don't allow insertions in the PDB.
+    Sabdab uses insertions for the chothia numbering so we need to allow them.
+
+    Took out lines 110-112 since that would mess up CDR numbering.
+
+    Args:
+        chain: Instance of Biopython's chain class.
+
+    Returns:
+        Protein object with protein features.
+    """
+    atom_positions = []
+    aatype = []
+    atom_mask = []
+    residue_index = []
+    b_factors = []
+    chain_ids = []
+    for res in chain:
+        res_shortname = residue_constants.restype_3to1.get(res.resname, 'X')
+        restype_idx = residue_constants.restype_order.get(
+            res_shortname, residue_constants.restype_num)
+        pos = np.zeros((residue_constants.atom_type_num, 3))
+        mask = np.zeros((residue_constants.atom_type_num,))
+        res_b_factors = np.zeros((residue_constants.atom_type_num,))
+        for atom in res:
+            if atom.name not in residue_constants.atom_types:
+                continue
+            pos[residue_constants.atom_order[atom.name]] = atom.coord
+            mask[residue_constants.atom_order[atom.name]] = 1.
+            res_b_factors[residue_constants.atom_order[atom.name]
+                          ] = atom.bfactor
+        aatype.append(restype_idx)
+        atom_positions.append(pos)
+        atom_mask.append(mask)
+        residue_index.append(res.id[1])
+        b_factors.append(res_b_factors)
+        chain_ids.append(chain_id)
+
+    return Protein(
+        atom_positions=np.array(atom_positions),
+        atom_mask=np.array(atom_mask),
+        aatype=np.array(aatype),
+        residue_index=np.array(residue_index),
+        chain_index=np.array(chain_ids),
+        b_factors=np.array(b_factors))
+
+     
+def calc_tm_score(pos_1, pos_2, seq_1, seq_2):
+    #https://github.com/jasonkyuyim/se3_diffusion/blob/master/analysis/metrics.py
+    tm_results = tm_align(pos_1, pos_2, seq_1, seq_2)
+    return tm_results.tm_norm_chain1, tm_results.tm_norm_chain2
+
+
+def calc_aligned_rmsd(pos_1, pos_2):
+    #https://github.com/jasonkyuyim/se3_diffusion/blob/master/analysis/metrics.py
+    aligned_pos_1 = rigid_transform_3D(pos_1, pos_2)[0]
+    return np.mean(np.linalg.norm(aligned_pos_1 - pos_2, axis=-1))
+
+
+def rigid_transform_3D(A, B, verbose=False):
+    # Transforms A to look like B
+    # https://github.com/nghiaho12/rigid_transform_3D
+    assert A.shape == B.shape
+    A = A.T
+    B = B.T
+
+    num_rows, num_cols = A.shape
+    if num_rows != 3:
+        raise Exception(f"matrix A is not 3xN, it is {num_rows}x{num_cols}")
+
+    num_rows, num_cols = B.shape
+    if num_rows != 3:
+        raise Exception(f"matrix B is not 3xN, it is {num_rows}x{num_cols}")
+
+    # find mean column wise
+    centroid_A = np.mean(A, axis=1)
+    centroid_B = np.mean(B, axis=1)
+
+    # ensure centroids are 3x1
+    centroid_A = centroid_A.reshape(-1, 1)
+    centroid_B = centroid_B.reshape(-1, 1)
+
+    # subtract mean
+    Am = A - centroid_A
+    Bm = B - centroid_B
+
+    H = Am @ np.transpose(Bm)
+
+    # sanity check
+    #if linalg.matrix_rank(H) < 3:
+    #    raise ValueError("rank of H = {}, expecting 3".format(linalg.matrix_rank(H)))
+
+    # find rotation
+    U, S, Vt = np.linalg.svd(H)
+    R = Vt.T @ U.T
+
+    # special reflection case
+    reflection_detected = False
+    if np.linalg.det(R) < 0:
+        if verbose:
+            print("det(R) < R, reflection detected!, correcting for it ...")
+        Vt[2,:] *= -1
+        R = Vt.T @ U.T
+        reflection_detected = True
+
+    t = -R @ centroid_A + centroid_B
+    optimal_A = R @ A + t
+
+    return optimal_A.T, R, t, reflection_detected
